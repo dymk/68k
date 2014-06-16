3eaa0a6dd49962c83553021250d32169a1609800
Dylan Knutson <tcdknutson@gmail.com>
2014-06-15 20:54:45 -0700

    port zzj's to 328 w/ shift register

--- flasher/flasher.ino
+++ flasher/flasher.ino
@@ -1,230 +1,350 @@
-/*
-  Programmer for 39SF040 flash
-*/
+#include <stdarg.h>
+#include <stdint.h>
 
-/*
-addressing shift registers pins -> pins on flash
-SR clock: PIN2, PORTD << 2 (orange) (red probe, ch 0)
-SR latch: PIN3, PORTD << 3 (blue)   (brown probe, ch 1)
-SR data:  PIN4, PORTD << 4 (yellow) (yellow probe, ch 2)
-
-first SR:
-5 => 18
-6 => 17
-7 => 16
-
-second SR:
-0 => 15
-1 => 14
-2 => 13
-3 => 12
-4 => 11
-5 => 10
-6 => 9
-7 => 8
-
-third SR:
-0 => 7
-1 => 6
-2 => 5
-3 => 4
-4 => 3
-5 => 2
-6 => 1
-7 => 0
-
-Mappings for the Flash:
-#CE => PINC.2, pin A0 (red jumper)
-#OE => PINC.3, pin A1 (blue jumper)
-#WE => PINC.4, pin A2 (yellow jumper)
-*/
-// chip enable
-#define CE A0
-// read
-#define OE A1
-// write
-#define WE A2
-
-#include "lib.h"
-
+// shift register pin defines
 #define SR_CLK 2
 #define SR_LATCH 3
 #define SR_DATA 4
 
-#define DEBUG 1
+// control lines for the flash chip
+#define CS A0
+#define RD A1
+#define WR A2
 
-// the setup routine runs once when you press reset:
-void setup() {
-	Serial.begin(9600);
+void _printf(char *fmt, ... ){
+  char buf[128]; // resulting string limited to 128 chars
+  va_list args;
+  va_start (args, fmt );
+  vsnprintf(buf, 128, fmt, args);
+  va_end (args);
+  Serial.print(buf);
+}
 
-	// initialize the digital pin as an output.
-	pinMode(SR_CLK, OUTPUT);
-	pinMode(SR_LATCH, OUTPUT);
-	pinMode(SR_DATA, OUTPUT);
+enum {
+  READY = 'A', // a
+  OKAY,        // b
+  UNKNOWN_CMD, // c
+  BAD_INPUT,   // d
+  CRC_ERROR,   // e
+  WRITE_ERROR, // f
+  TIMEOUT,     // g
+  ERASE_FAIL   // h
+};
 
-	pinMode(CE, OUTPUT);
-	pinMode(OE, OUTPUT);
-	pinMode(WE, OUTPUT);
+uint8_t error = 0;
 
-	// disable flash chip
-	digitalWrite(CE, HIGH);
-	digitalWrite(OE, HIGH);
-	digitalWrite(WE, HIGH);
+int getCh() {
+  if (error) return 0;
+  uint32_t st = millis();
+  while (!Serial.available())  {
+     if (millis() - st > 1000) {
+       error = TIMEOUT;
+       return 0;
+     }
+  }
+  return Serial.read();
+}
 
-	// sanity checks
-	if(sizeof(uint16_t) != 2)
-		Serial.println("uint16_t is not 2 bytes");
+uint16_t crc16_update(uint16_t crc, uint8_t a) {
+    int i;
+
+    crc ^= a;
+    for (i = 0; i < 8; ++i)
+    {
+        if (crc & 1)
+            crc = (crc >> 1) ^ 0xA001;
+        else
+            crc = (crc >> 1);
+    }
+
+    return crc;
+}
+
+uint8_t hex_to_byte(uint8_t ch) {
+    if (ch >= '0' && ch <= '9')
+        return ch - '0';
+
+    ch = ch & 0xDF;
+
+    if (ch >= 'A' && ch <= 'F')
+        return ch - 'A' + 10;
+
+   // _printf("Bad hex char: %c (%d)\n",ch,ch);
+    if (error == 0)
+      error = BAD_INPUT;
+    return 0;
+}
+
+uint8_t readbyte() {
+    int ch1 = getCh();
+    int ch2 = getCh();
+
+    ch1 = hex_to_byte(ch1);
+    ch2 = hex_to_byte(ch2);
+
+    return ch1 << 4 | ch2;
+}
+
+uint16_t readshort() {
+    return (((uint16_t)readbyte()) << 8) | readbyte();
+}
+
+uint32_t read3bint() {
+    return (((uint32_t)readbyte()) << 16) | (((uint32_t)readbyte()) << 8) | readbyte();
+}
+
+uint32_t readint() {
+    return (((uint32_t)readshort()) << 8) | readshort();
 }
 
 // Sets the address bus
-void set_addr(uint32_t addr) {
+void set_address(uint32_t addr) {
 #if DEBUG
-	Serial.print("Setting address to: 0x");
-	Serial.println(addr, HEX);
+  Serial.print("Setting address to: 0x");
+  Serial.println(addr, HEX);
 #endif
 
-	// bring latch low
-	digitalWrite(SR_LATCH, LOW);
+  // bring latch low
+  digitalWrite(SR_LATCH, LOW);
 
-	// 24 bits of addr, as three SRs are used
-	for(unsigned int i = 0; i < 24; i++) {
+  // 24 bits of addr, as three SRs are used
+  for(unsigned int i = 0; i < 24; i++) {
 
-		// write the bit
-		digitalWrite(SR_DATA, (addr >> i) & 1);
+    // write the bit
+    digitalWrite(SR_DATA, (addr >> i) & 1);
 
-		// toggle clock
-		digitalWrite(SR_CLK, LOW);
-		digitalWrite(SR_CLK, HIGH);
-	}
+    // toggle clock
+    digitalWrite(SR_CLK, LOW);
+    digitalWrite(SR_CLK, HIGH);
+  }
 
-	// and toggle latch to write it to the output
-	digitalWrite(SR_CLK, LOW);
-	digitalWrite(SR_LATCH, HIGH);
-	digitalWrite(SR_LATCH, LOW);
+  // and toggle latch to write it to the output
+  digitalWrite(SR_CLK, LOW);
+  digitalWrite(SR_LATCH, HIGH);
+  digitalWrite(SR_LATCH, LOW);
 
-	Serial.println("");
+  Serial.println("");
+}
+
+void do_write(uint32_t addr,byte b) {
+  set_address(addr);
+  set_data(b);
+
+  digitalWrite(CS, 0);
+  digitalWrite(WR, 0);
+
+  digitalWrite(WR, 1);
+  digitalWrite(CS, 1);
 }
 
 // Sets 'data' to the databus
 void set_data(uint8_t data) {
-	// lower 6 bits is PORTB0-5,
-	// upper nibble is PORTC0-3
+  // lower 6 bits is PORTB0-5,
+  // upper nibble is PORTC0-3
 
 #if DEBUG
-	Serial.print("Writing to data: 0x");
-	Serial.println(data, HEX);
+  Serial.print("Writing to data: 0x");
+  Serial.println(data, HEX);
 #endif
 
-	DDRB |= 0x3F; // 0b111111
-	DDRC |= 0x3;  // 0b11
+  DDRB |= 0x3F; // 0b111111
+  DDRC |= 0x3;  // 0b11
 
-	PORTB = data & 0x3F;
-	PORTC = (data >> 6) & 0x3;
+  PORTB = data & 0x3F;
+  PORTC = (data >> 6) & 0x3;
 }
 
 // Reads a byte from the databus
 uint8_t read_data() {
-	DDRB &= ~0x3F;
-	DDRC &= ~0x3;
+  DDRB &= ~0x3F;
+  DDRC &= ~0x3;
 
-	uint8_t ret;
-	ret = ((PINC & 0x3) << 6) | (PINB & 0x3F);
+  uint8_t ret;
+  ret = ((PINC & 0x3) << 6) | (PINB & 0x3F);
 
-	return ret;
+  return ret;
 }
 
 // Read the data at address 'addr'
-uint8_t read_from_addr(uint32_t addr) {
-	set_addr(addr);
-	asm("nop\n nop\n");
+uint8_t do_read(uint32_t addr) {
+  set_address(addr);
 
-	digitalWrite(CE, LOW);
-	digitalWrite(OE, LOW);
-	asm("nop\n nop\n");
+  digitalWrite(CS, LOW);
+  digitalWrite(RD, LOW);
+  asm("nop\nnop\n");
 
-	uint8_t ret = read_data();
-	digitalWrite(CE, HIGH);
-	digitalWrite(OE, HIGH);
+  uint8_t ret = read_data();
+  digitalWrite(CS, HIGH);
+  digitalWrite(RD, HIGH);
 
-	return ret;
+  return ret;
 }
 
-// Latches the address and data for the flash
-void latch_addr_and_data(uint32_t addr, uint8_t data) {
-	set_addr(addr);
-	asm("nop\nnop\n");
-
-	digitalWrite(CE, LOW);
-	digitalWrite(WE, LOW);
-	asm("nop\nnop\n");
-
-	set_data(data);
-	digitalWrite(CE, HIGH);
-	digitalWrite(WE, HIGH);
+void write_byte(uint32_t addr, uint8_t b) {
+  // data protection sequence
+  do_write(0x5555,0xAA);
+  do_write(0x2AAA,0x55);
+  do_write(0x5555,0xA0);
+  do_write(addr, b);
+  delayMicroseconds(50); // should not take longer than this to write a byte
 }
 
-// Writes data to the flash at address
-void write_to_addr(uint32_t addr, uint8_t data) {
-	latch_addr_and_data(0x5555UL, 0xAA);
-	latch_addr_and_data(0x2AAAUL, 0x55);
-	latch_addr_and_data(0x5555UL, 0xA0);
-	latch_addr_and_data(addr, data);
+uint8_t buffer[1024];
 
-	// spec says 20uS to write the data internally
-	delayMicroseconds(20);
+uint8_t write_buffer(uint32_t addr, uint16_t sz) {
+  byte bin, bout;
+
+  for (uint16_t i = 0; i < sz; i++) {
+    bin = buffer[i];
+    if (bin == 0xFF) {  addr++;  continue; } // no need to write 0xFF when chip is erased
+
+    write_byte(addr, bin);
+
+    // try to write up to three times
+    if (bin != do_read(addr)) {
+      write_byte(addr, bin);
+      if (bin != do_read(addr)) {
+        write_byte(addr, bin);
+        if (bin != do_read(addr)) {
+          write_byte(addr, bin);
+          return 0;
+        }
+      }
+    }
+
+    addr++;
+  }
+
+  return 1;
 }
 
-// Erase the flash chip
-void erase_chip() {
-	latch_addr_and_data(0x5555UL,0xAA);
-	latch_addr_and_data(0x2AAAUL,0x55);
-	latch_addr_and_data(0x5555UL,0x80);
-	latch_addr_and_data(0x5555UL,0xAA);
-	latch_addr_and_data(0x2AAAUL,0x55);
-	latch_addr_and_data(0x5555UL,0x10);
+uint8_t chip_erase() {
+  do_write(0x5555,0xAA);
+  do_write(0x2AAA,0x55);
+  do_write(0x5555,0x80);
+  do_write(0x5555,0xAA);
+  do_write(0x2AAA,0x55);
+  do_write(0x5555,0x10);
 
-	delay(100);
+  delay(100);
+  return 1;
 }
 
-// the loop routine runs over and over again forever:
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+
+  // initialize the digital pin as an output.
+  pinMode(SR_CLK, OUTPUT);
+  pinMode(SR_LATCH, OUTPUT);
+  pinMode(SR_DATA, OUTPUT);
+
+  pinMode(CS, OUTPUT);
+  pinMode(RD, OUTPUT);
+  pinMode(WR, OUTPUT);
+
+  // disable flash chip
+  digitalWrite(CS, HIGH);
+  digitalWrite(RD, HIGH);
+  digitalWrite(WR, HIGH);
+}
+
+
+void do_dump (uint32_t addr, uint32_t cnt) {
+  int c = 0;
+  char ascii[17];
+  ascii[16] = 0;
+
+  Serial.println();
+
+  _printf("%6lX   ", addr);
+  for (uint32_t i = 0; i < cnt; i++) {
+    uint8_t b = do_read(addr);
+
+    ascii[c] = (b > 31 & b < 127) ? b : '.';
+
+    _printf("%02X ",b);
+
+    addr++;
+
+    if (++c == 16) {
+      Serial.print("  ");
+      Serial.print(ascii);
+      Serial.println();
+      if ((i+1) < cnt)
+        _printf("%6lX   ", addr);
+      c = 0;
+    }
+  }
+
+  if (c < 15) Serial.println();
+
+  Serial.write(4);
+}
+
 void loop() {
-	Serial.write(READY);
-	while(!Serial.available());
-	Request req = (Request) Serial.read();
+  Serial.write(READY);
+  while (!Serial.available());
+  int ch = Serial.read();
 
-	uint8_t data;
-	uint32_t addr;
-	char buff[10];
+  if (ch == 'H') {
+     Serial.write('T');
+     Serial.write(OKAY);
+  } else if (ch == 'E') {
+     if (chip_erase()) {
+       Serial.write(OKAY);
+     } else
+       Serial.write(ERASE_FAIL);
+  } else if (ch == 'A') {
+    Serial.write(OKAY);
+    for (uint32_t addr = 0; addr < 0x80000; addr++)
+      Serial.write(do_read(addr));
+    Serial.write(OKAY);
+  } else if (ch == 'R') {
+    error = 0;
+    uint32_t addr = read3bint();
+    uint16_t cnt = read3bint();
 
-	switch(req) {
-		case SET_DATA:
-			data = (uint8_t) Serial.parseInt();
-			set_data(data);
-			Serial.write(SUCCESS);
-			break;
+    if (error) {
+      addr = 0x0;
+      cnt = 512;
+    }
 
-		case PING:
-			Serial.write(PONG);
-			break;
+    do_dump(addr,cnt);
+    Serial.write(OKAY);
+  } else if (ch == 'D') {
+      error = 0;
+      uint32_t addr = read3bint();
+      uint16_t sz = readshort();
 
-		case ERASE:
-			if(erase_chip()) {
-				Serial.write(SUCCESS);
-			} else {
-				Serial.write(FAIL);
-			}
-			break;
+      uint16_t mycrc = 0;
+       for (uint16_t i = 0; i < sz; i++) {
+          buffer[i] =  readbyte();
+          mycrc = crc16_update(mycrc,buffer[i]);
+      }
 
-		// Sets the address bus to the recieved 16 bit addr
-		case SET_ADDR:
-			Serial.readBytesUntil('\n', buff, 10);
-			sscanf(buff, "%lu", &addr);
-			set_addr(addr);
-			Serial.write(SUCCESS);
-			break;
+      uint16_t crc = readshort();
 
-		default:
-			Serial.write(UNKNOWN);
-			break;
-	}
+      if (error) {
+        Serial.write(error);
+        return;
+      }
+
+      /*Serial.print(addr, HEX);
+      _printf(" sz %4X ",sz);
+      _printf("crc %4X ",crc);
+      _printf("(ours %4X)",mycrc);*/
+
+      if (mycrc != crc) {
+        Serial.write(CRC_ERROR);
+        return;
+      }
+
+      if (write_buffer(addr, sz)) {
+        Serial.write(OKAY);
+      } else
+        Serial.write(WRITE_ERROR);
+  } else {
+      Serial.write(UNKNOWN_CMD);
+  }
 }
